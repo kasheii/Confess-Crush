d3d4560ea9ddc8ab491e66cb992e5dfad78cc842
kasheii <106745790+kasheii@users.noreply.github.com>
2022-06-02 19:46:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Chào emiu",
-    introTitle: 'Hzang cuteee',
+    titleWeb: "Chào bạn",
+    introTitle: 'My friend',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại chơi với mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Tại vì TDat đẹp trai',
+    mess: 'Đừng khen nữa ngại lắn :))',
+    messDesc: 'Chúc bạn buổi tối vui vẻ.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/kasheii/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/kasheii/Confess-Friend' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
